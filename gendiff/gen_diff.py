efced9a99a80f1debe0cd274778a01dd946bcdb6
Ilya Bagrov <ilya.b.only@gmail.ru>
2023-04-09 01:37:46 +0300

    Fix diff's layout

--- gendiff/gen_diff.py
+++ gendiff/gen_diff.py
@@ -5,22 +5,23 @@ def generate_diff(file_path1, file_path2):
     file1 = json.load(open(file_path1))  # dict
     file2 = json.load(open(file_path2))
 
-    keys1 = set(file1.keys())
-    keys2 = set(file2.keys())
-    common_keys = keys1 & keys2
+    keys = list(file1.keys())
+    keys2 = list(file2.keys())
+    keys.extend(keys2)
+    keys.sort()
 
     diff = {}
-    for key in common_keys:
-        if file1[key] == file2[key]:
-            diff[key] = file1[key]
+    for key in keys:
+        if key in file1:
+            if key in file2:
+                if file1[key] == file2[key]:
+                    diff[f'  {key}'] = file1[key]
+                else:
+                    diff[f'- {key}'] = file1[key]
+                    diff[f'+ {key}'] = file2[key]
+            else:
+                diff[f'- {key}'] = file1[key]
         else:
-            diff[f'- {key}'] = file1[key]
             diff[f'+ {key}'] = file2[key]
-        keys1.remove(key)
-        keys2.remove(key)
-    for key1 in keys1:
-        diff[f'- {key1}'] = file1[key]
-    for key2 in keys2:
-        diff[f'+ {key2}'] = file2[key]
-    
-    return diff
+
+    return json.dumps(diff, indent=2).replace('"', '')    
