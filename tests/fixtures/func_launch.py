b89217e44dc99d078d4d07ce8b20d7e5d22eb3f0
Ilya Bagrov <ilya.b.only@gmail.ru>
2023-04-18 00:28:21 +0300

    Add check of both json and yaml formats

--- tests/fixtures/func_launch.py
+++ tests/fixtures/func_launch.py
@@ -1,4 +1,5 @@
 from gendiff import generate_diff
 
 
-print(generate_diff('tests/fixtures/file1.json', 'tests/fixtures/file2.json'))
+print(generate_diff('tests/fixtures/JSON/file1.json', 'tests/fixtures/JSON/file2.json'))
+print(generate_diff('tests/fixtures/YAML/file1.yml', 'tests/fixtures/YAML/file2.yml'))
--- tests/test_gen_diff.py
+++ tests/test_gen_diff.py
@@ -1,10 +1,21 @@
 from gendiff.gen_diff import generate_diff
 
 
-file1 = 'tests/fixtures/file1.json'
-file2 = 'tests/fixtures/file2.json'
+file1_json = 'tests/fixtures/JSON/file1.json'
+file2_json = 'tests/fixtures/JSON/file2.json'
+file1_yaml = 'tests/fixtures/YAML/file1.yml'
+file2_yaml = 'tests/fixtures/YAML/file2.yml'
 
-diff = open('tests/fixtures/correct_diff.txt')
+correct_diff = open('tests/fixtures/correct_diff.txt').read()
+wrong_diff = open('tests/fixtures/wrong_diff.txt').read()
 
-def test_generate_diff():
-    assert generate_diff(file1, file2) == diff.read()
+
+def test_generate_diff_json():
+    diff = generate_diff(file1_json, file2_json)
+    assert  diff == correct_diff
+    assert diff != wrong_diff
+
+def test_generate_diff_yaml():
+    diff = generate_diff(file1_yaml, file2_yaml)
+    assert  diff == correct_diff
+    assert diff != wrong_diff
