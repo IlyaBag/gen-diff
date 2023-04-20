f03166c2664aa4e45df29994a4c7b304325afff3
Ilya Bagrov <ilya.b.only@gmail.ru>
2023-04-21 00:35:17 +0300

    Add YAML comparison functionality, change version to 0.3.0

--- gendiff/gen_diff.py
+++ gendiff/gen_diff.py
@@ -1,35 +1,41 @@
-from gendiff.parser import get_data, show_diff
+import json
+import yaml
+from gendiff.parser import show_diff
+
+
+def file_parse(path):
+    file_extension = path.rsplit(sep='.', maxsplit=1)[1]
+    file_extension.lower()
+
+    data = {}
+    if file_extension == 'json':
+        data = json.load(open(path))
+    elif file_extension == 'yml' or file_extension == 'yaml':
+        data = yaml.load(open(path), Loader=yaml.Loader)
+    return data
 
 
 def generate_diff(file_path1, file_path2):
-    file1, file2, file_extension = get_data(file_path1, file_path2)
+    file1 = file_parse(file_path1)
+    file2 = file_parse(file_path2)
 
     common_data = set(file1.keys()) | set(file2.keys())
     keys = list(common_data)
     keys.sort()
 
-    # file1 = json.load(open(file_path1))  # dict
-    # file2 = json.load(open(file_path2))
-
-    # keys = list(file1.keys())
-    # keys2 = list(file2.keys())
-    # keys.extend(keys2)
-    # keys.sort()
-
-    diff = {}
+    diff = []
     for key in keys:
         if key in file1:
             if key in file2:
                 if file1[key] == file2[key]:
-                    diff[f'  {key}'] = file1[key]
+                    diff.append((f'  {key}', f'{file1[key]}'))
                 else:
-                    diff[f'- {key}'] = file1[key]
-                    diff[f'+ {key}'] = file2[key]
+                    diff.append((f'- {key}', f'{file1[key]}'))
+                    diff.append((f'+ {key}', f'{file2[key]}'))
             else:
-                diff[f'- {key}'] = file1[key]
+                diff.append((f'- {key}', f'{file1[key]}'))
         else:
-            diff[f'+ {key}'] = file2[key]
+            diff.append((f'+ {key}', f'{file2[key]}'))
 
-    # return json.dumps(diff, indent=2).replace('"', '')
-    printable_diff = show_diff(diff, file_extension)
+    printable_diff = show_diff(diff)
     return printable_diff
--- gendiff/parser.py
+++ gendiff/parser.py
@@ -1,39 +1,12 @@
-import json
-import yaml
+def fix_syntax(string):
+    if string in ('True', 'False'):
+        return string.lower()
+    return string
 
 
-# def get_file_extension(path):
-#     file_extension = path.rsplit(sep='.', maxsplit=1)[1]
-#     return file_extension.lower()
-def file_parse(path):
-    file_extension = path.rsplit(sep='.', maxsplit=1)[1]
-    file_extension.lower()
-
-    data = {}
-    if file_extension == 'json':
-        data = json.load(open(path))
-    elif file_extension == 'yml' or file_extension == 'yaml':
-        data = yaml.load(open(path), Loader=yaml.Loader)
-    return data, file_extension
-
-
-def get_data(file_path1, file_path2):
-    file1, file1_ext = file_parse(file_path1)
-    file2, file2_ext = file_parse(file_path2)
-    file_extension = 'yaml'
-    if file1_ext == 'json' and file2_ext == 'json':
-        file_extension = 'json'
-    return file1, file2, file_extension
-
-
-def show_diff(diff, file_format):
-    if file_format == 'json':
-        return json.dumps(diff, indent=2).replace('"', '')
-    elif file_format == 'yaml':
-        return yaml.dump(diff,
-                         default_flow_style=False,
-                         width=float('INF'),
-                         sort_keys=False
-                         ).replace("'", "")
-    else:
-        return 'Unknown file format'
+def show_diff(diff):
+    printable_diff = '{'
+    for pair in diff:
+        printable_diff += f'\n  {pair[0]}: {fix_syntax(pair[1])}'
+    printable_diff += '\n}'
+    return printable_diff
