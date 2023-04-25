31a4cc30f34d068f20ab72017979e2424bf3e424
Ilya Bagrov <ilya.b.only@gmail.ru>
2023-04-26 00:47:12 +0300

    Intermediate state 	изменено:      gendiff/gen_diff.py 	изменено:      gendiff/parser.py

--- gendiff/gen_diff.py
+++ gendiff/gen_diff.py
@@ -19,23 +19,42 @@ def generate_diff(file_path1, file_path2):
     file1 = file_parse(file_path1)
     file2 = file_parse(file_path2)
 
-    common_data = set(file1.keys()) | set(file2.keys())
-    keys = list(common_data)
-    keys.sort()
+    def inner(file1, file2):
+        common_data = set(file1.keys()) | set(file2.keys())
+        keys = list(common_data)
+        keys.sort()
 
-    diff = []
-    for key in keys:
-        if key in file1:
-            if key in file2:
-                if file1[key] == file2[key]:
-                    diff.append((f'  {key}', f'{file1[key]}'))
+        diff = {}
+        for key in keys:
+            if key in file1:
+                if key in file2:
+                    if file1[key] == file2[key]:
+                        diff[f'  {key}'] = f'{file1[key]}'
+                    else:
+                        if isinstance(file1[key], dict) and isinstance(file2[key], dict):
+                            inner_diff = inner(file1[key], file2[key])
+                            diff[f'  {key}'] = inner_diff
+                        else:
+                            diff[f'- {key}'] = f'{file1[key]}'
+                            diff[f'+ {key}'] = f'{file2[key]}'
                 else:
-                    diff.append((f'- {key}', f'{file1[key]}'))
-                    diff.append((f'+ {key}', f'{file2[key]}'))
+                    diff[f'- {key}'] = f'{file1[key]}'
             else:
-                diff.append((f'- {key}', f'{file1[key]}'))
-        else:
-            diff.append((f'+ {key}', f'{file2[key]}'))
+                diff[f'+ {key}'] = f'{file2[key]}'
+    # diff = []
+    # for key in keys:
+    #     if key in file1:
+    #         if key in file2:
+    #             if file1[key] == file2[key]:
+    #                 diff.append((f'  {key}', f'{file1[key]}'))
+    #             else:
+    #                 diff.append((f'- {key}', f'{file1[key]}'))
+    #                 diff.append((f'+ {key}', f'{file2[key]}'))
+    #         else:
+    #             diff.append((f'- {key}', f'{file1[key]}'))
+    #     else:
+    #         diff.append((f'+ {key}', f'{file2[key]}'))
 
-    printable_diff = show_diff(diff)
-    return printable_diff
+        # printable_diff = show_diff(diff)
+        return diff
+    return show_diff(inner(file1, file2))
--- gendiff/parser.py
+++ gendiff/parser.py
@@ -1,12 +1,27 @@
 def fix_syntax(string):
     if string in ('True', 'False'):
         return string.lower()
+    if string == 'None':
+        return 'null'
     return string
 
 
-def show_diff(diff):
-    printable_diff = '{'
-    for pair in diff:
-        printable_diff += f'\n  {pair[0]}: {fix_syntax(pair[1])}'
-    printable_diff += '\n}'
-    return printable_diff
+def show_diff(diff, replacer=' ', indent_length=4):
+    def inner(cur_diff, accum_indent):
+        cur_indent_length = accum_indent + indent_length
+        cur_indent = replacer * (cur_indent_length - 2)
+        printable_diff = '{'
+        for key in cur_diff:
+            if isinstance(cur_diff[key], dict):
+                # может отдельную функцию для словарей?
+                printable_diff += f'\n{cur_indent}{key}: {inner(cur_diff[key], cur_indent_length)}'
+            else:
+                printable_diff += f'\n{cur_indent}{key}: {fix_syntax(cur_diff[key])}'
+        printable_diff += '\n' + replacer * accum_indent + '}'
+        return printable_diff
+    return inner(diff, 0)
+    # printable_diff = '{'
+    # for pair in diff:
+    #     printable_diff += f'\n  {pair[0]}: {fix_syntax(pair[1])}'
+    # printable_diff += '\n}'
+    # return printable_diff
