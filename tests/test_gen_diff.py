637880e72284fa2e7566c222cdd68437b0524440
Ilya Bagrov <ilya.b.only@gmail.ru>
2023-04-26 00:46:57 +0300

    Update tests with a nested data

--- tests/test_gen_diff.py
+++ tests/test_gen_diff.py
@@ -1,13 +1,18 @@
 from gendiff.gen_diff import generate_diff
 
 
-file1_json = 'tests/fixtures/JSON/file1.json'
-file2_json = 'tests/fixtures/JSON/file2.json'
-file1_yaml = 'tests/fixtures/YAML/file1.yml'
-file2_yaml = 'tests/fixtures/YAML/file2.yml'
+file1_json = 'tests/fixtures/flat/file1.json'
+file2_json = 'tests/fixtures/flat/file2.json'
+file1_yaml = 'tests/fixtures/flat/file1.yml'
+file2_yaml = 'tests/fixtures/flat/file2.yml'
+file1_json_nested = 'tests/fixtures/nested/file1.json'
+file2_json_nested = 'tests/fixtures/nested/file2.json'
+file1_yaml_nested = 'tests/fixtures/nested/file1.yml'
+file2_yaml_nested = 'tests/fixtures/nested/file2.yml'
 
 correct_diff = open('tests/fixtures/correct_diff.txt').read()
 wrong_diff = open('tests/fixtures/wrong_diff.txt').read()
+correct_nested_diff = open('tests/fixtures/correct_nested_diff.txt').read()
 
 
 def test_generate_diff_json():
@@ -19,3 +24,11 @@ def test_generate_diff_yaml():
     diff = generate_diff(file1_yaml, file2_yaml)
     assert  diff == correct_diff
     assert diff != wrong_diff
+
+def test_generate_diff_json_nested():
+    diff = generate_diff(file1_json_nested, file2_json_nested)
+    assert  diff == correct_nested_diff
+
+def test_generate_diff_yaml_nested():
+    diff = generate_diff(file1_yaml_nested, file2_yaml_nested)
+    assert  diff == correct_nested_diff
