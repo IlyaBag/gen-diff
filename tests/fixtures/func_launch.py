9093f7aa2e842657c79011ee7600d28c35b95dea
Ilya Bagrov <ilya.b.only@gmail.ru>
2023-04-26 00:46:15 +0300

    Update fixtures with a nested data

--- tests/fixtures/func_launch.py
+++ tests/fixtures/func_launch.py
@@ -2,7 +2,13 @@ from gendiff import generate_diff
 
 
 print('json')
-print(generate_diff('tests/fixtures/JSON/file1.json', 'tests/fixtures/JSON/file2.json'))
+print(generate_diff('tests/fixtures/flat/file1.json', 'tests/fixtures/flat/file2.json'))
 print()
 print('yaml')
-print(generate_diff('tests/fixtures/YAML/file1.yml', 'tests/fixtures/YAML/file2.yml'))
+print(generate_diff('tests/fixtures/flat/file1.yml', 'tests/fixtures/flat/file2.yml'))
+print()
+print('nested json')
+print(generate_diff('tests/fixtures/nested/file1.json', 'tests/fixtures/nested/file2.json'))
+print()
+print('nested yaml')
+print(generate_diff('tests/fixtures/nested/file1.yml', 'tests/fixtures/nested/file2.yml'))
