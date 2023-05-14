a407c1173379c3b2eb50b6f644d67073e0b11faa
Ilya Bagrov <ilya.b.only@gmail.ru>
2023-05-15 01:22:15 +0300

    Fix "stylish" (in progress again)

--- gendiff/formatters/f_stylish.py
+++ gendiff/formatters/f_stylish.py
@@ -1,25 +1,31 @@
-STATES = {'deleted': '-',
-          'added': '+',
-          'same': ' ',
-          'changed': '',  # здесь должа быть функция??
-          'nested': ' '}
+STATES = {'deleted': '- ',
+          'added': '+ ',
+          'same': '  ',
+          'changed_from': '- ',  # здесь должа быть функция??
+          'changed_to': '+ ',  # здесь должа быть функция??
+          'nested': '  '}
 
 
 def stylish(data, replacer=' ', indent_length=4) -> str:
     if not isinstance(data, dict):
-        return get_printable_value(data)  # переводим значение в строку
+        return str(data)  # get_printable_value(data)  # переводим значение в строку
     
     keys = list(data.keys())
     keys.sort()
 
     printable_diff = '{'
 
+    indent = replacer * (indent_length - 2)
     for key in keys:
-        state, value = data[key]
-        nested_value = stylish(value, replacer, indent_length)
-        
-        # f'{state_char} {key}: {value}'
+        diff_key, state = key
+        raw_value = data[key]
+        value = stylish(raw_value, replacer, indent_length)
+        printable_diff += f'\n{indent}{STATES[state]}{diff_key}: {value}'
 
     printable_diff += '\n}'
 
     return printable_diff
+
+from tests.fixtures.mein_raw_diff_flat import diff
+
+print(stylish(diff))
